10a001cd87c97da9c702717d89d21aee15edf891
Hoooon22 <momo990305@gmail.com>
2021-09-07 23:40:00 +0900

    sync

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -35,6 +35,7 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.UpArrow)) // Jump
         {
             rigid.AddForce(new Vector2(0, playerSpeed), ForceMode2D.Force);
+
         }
     }
 }
