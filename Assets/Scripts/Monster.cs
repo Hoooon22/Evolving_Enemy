4ca55e617fec51c8531f0aacded73b9d8a353833
Hoooon22 <momo990305@gmail.com>
2021-10-03 20:55:33 +0900

    Monster Chase Complete

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
-    int monsterSpeed = 100; 
+    int monsterSpeed = 70; 
 
     Rigidbody2D rigid;
     SpriteRenderer rend;
     GameObject player;
-    Vector2 pos;
 
     void Start()
     {
@@ -17,33 +16,10 @@ public class Monster : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
         rend = GetComponent<SpriteRenderer>();
         player = GameObject.FindGameObjectWithTag("Player");
-
-        pos = this.gameObject.transform.position;
-    }
-
-    void FixedUpdate()
-    {
-        MonsterMove();
     }
 
     void Update()
     {
-        
-    }
-
-    void MonsterMove()
-    {
-        if (pos.x == player.transform.position.x)
-        {
-            rigid.AddForce(new Vector2(0, 0), ForceMode2D.Force);
-        }
-        else if (pos.x > player.transform.position.x)
-        {
-            rigid.AddForce(new Vector2(-monsterSpeed, 0), ForceMode2D.Force);
-        }
-        else if (pos.x < player.transform.position.x)
-        {
-            rigid.AddForce(new Vector2(monsterSpeed, 0), ForceMode2D.Force);
-        }
+        transform.position = Vector2.MoveTowards(gameObject.transform.position, player.transform.position, 0.1f);
     }
 }
