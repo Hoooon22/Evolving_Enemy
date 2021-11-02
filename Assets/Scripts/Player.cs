3954d7521d7b7755e75149f14c0fa997fa9b2131
Hoooon22 <momo990305@gmail.com>
2021-11-02 21:35:47 +0900

    init scripts

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,54 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    int playerSpeed = 100; // player speed
-    int PlayerJumpPower = 100; // player jump power
-    int jumpCount = 1; // jump count
-
-    Rigidbody2D rigid;
-    SpriteRenderer rend;
-    
     void Start()
     {
-        // init
-        rigid = GetComponent<Rigidbody2D>(); 
-        rend = GetComponent<SpriteRenderer>();
+
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        PlayerMove();
-        PlayerJump();
-    }
-
-    void PlayerMove()
-    {
-        if (Input.GetKey(KeyCode.RightArrow)) // RightArrow
-        {
-            rend.flipX = false;
-            rigid.AddForce(new Vector2(playerSpeed, 0), ForceMode2D.Force);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow)) // LeftArrow
-        {
-            rend.flipX = true;
-            rigid.AddForce(new Vector2(-playerSpeed, 0), ForceMode2D.Force);
-        }
-    }
-    
-    void PlayerJump()
-    {
-        if (jumpCount > 0 && Input.GetKey(KeyCode.UpArrow)) // Jump
-        {
-            rigid.AddForce(new Vector2(0, PlayerJumpPower), ForceMode2D.Impulse);
-            jumpCount--;
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-            jumpCount = 1;
-        }
+        
     }
 }
